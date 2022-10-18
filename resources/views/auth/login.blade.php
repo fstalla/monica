51567a8180cbe85bb9646c98d29fcdff66226dee
Frank Stalla <fstalla@outlook.com>
2022-10-17 19:18:01 -0500

    test commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                 <img src="img/monica.svg" width="97" height="88" alt="">
               </div>
             </div>
-            <h2>{{ trans('auth.login_to_account') }}</h2>
+            <h2>{{ trans('auth.login_to_account') }} here</h2>
 
             @include ('partials.errors')
             @if (session('status'))
